6cbd1f0952aefbcbce0ca8a87950ca6b229611c1
Teresa Cereceda <tcereceda@gmail.com>
2020-08-29 19:02:16 +0200

    Initial version

--- Cronometro/MainWindow.xaml.cs
+++ Cronometro/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Cronometro.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Cronometro
 {
@@ -20,9 +22,50 @@ namespace Cronometro
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer dispatcherTimer;
+        Chrono chrono;
         public MainWindow()
         {
             InitializeComponent();
+
+            // Setup the timer
+            dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 1);
+
+            chrono = new Chrono();
         }
+        // When the timer tick
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            lblTimer.Content = chrono.ToString();
+        }
+
+        private void BtnStart_Click(object sender, RoutedEventArgs e)
+        {
+            chrono.Play();
+            dispatcherTimer.Start();
+ 
+        }
+
+        private void BtnPause_Click(object sender, RoutedEventArgs e)
+        {
+
+            chrono.Pause();
+            dispatcherTimer.Stop();
+
+        }
+
+        private void BtnStop_Click(object sender, RoutedEventArgs e)
+        {
+
+            chrono.Pause();
+            chrono.Reset();
+            dispatcherTimer.Stop();
+            lblTimer.Content = "00::00::00";
+        }
+
     }
-}
+    }
+
+   
